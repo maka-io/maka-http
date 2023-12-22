b73abee41653db5adfd3ecf6b5942babb8d111a8
maka-io <mjmcee@gmail.com>
2023-12-22 13:09:19 -0500

    ver

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'maka:http',
   summary: "Make HTTP calls to remote servers",
-  version: '1.0.4',
+  version: '1.0.5',
   git: 'https://github.com/maka-io/maka-http.git'
 });
 
